ed4e28e091d4cd6d6557c632f847b7cfdb0b6066
alexandra <alexandra@alexandra.analog.com>
2018-05-03 16:10:34 +0300

    Remove custom markdown and cheetah

--- CI/travis/before_install_linux.sh
+++ CI/travis/before_install_linux.sh
@@ -3,15 +3,17 @@
 set -e
 cd ${WORKDIR}
 
+apt-get install python-markdown python-cheetah libvolk1-dev
 mkdir -p deps
 cd deps
 WORKDIR=$PWD
 
 cd ${WORKDIR}
-rm boost_1_63_0.tar.gz*
-if [ ! -d boost_1_63_0 ]; then
+if [ ! -f boost_1_63_0.tar.gz ]; then
   wget https://netcologne.dl.sourceforge.net/project/boost/boost/1.63.0/boost_1_63_0.tar.gz
   tar -xzf boost_1_63_0.tar.gz
+fi;
+if [ ! -d boost_1_63_0 ]; then
   cd boost_1_63_0
   ./bootstrap.sh --with-libraries=date_time,filesystem,program_options,regex,system,test,thread >/dev/null
   ./b2 >/dev/null
@@ -20,43 +22,19 @@ else
 fi
 sudo ./b2 install >/dev/null
 
-cd ${WORKDIR}
-rm Markdown-2.6.8.tar.gz*
-if [ ! -d Markdown-2.6.8 ]; then
-  wget https://pypi.python.org/packages/1d/25/3f6d2cb31ec42ca5bd3bfbea99b63892b735d76e26f20dd2dcc34ffe4f0d/Markdown-2.6.8.tar.gz
-  tar -xzf Markdown-2.6.8.tar.gz
-  cd Markdown-2.6.8
-  ./setup.py build >/dev/null
-else
-  cd Markdown-2.6.8
-fi
-sudo ./setup.py install
-
-cd ${WORKDIR}
-rm Cheetah-2.4.4.tar.gz*
-if [ ! -d Cheetah-2.4.4 ]; then
-  wget https://pypi.python.org/packages/cd/b0/c2d700252fc251e91c08639ff41a8a5203b627f4e0a2ae18a6b662ab32ea/Cheetah-2.4.4.tar.gz
-  tar -xzf Cheetah-2.4.4.tar.gz
-  cd Cheetah-2.4.4
-  ./setup.py build >/dev/null
-else
-  cd Cheetah-2.4.4
-fi
-sudo ./setup.py install
-
-cd ${WORKDIR}
-rm volk-1.3.tar.gz*
-if [ ! -d volk-1.3 ]; then
-  wget http://libvolk.org/releases/volk-1.3.tar.gz
-  tar -xzf volk-1.3.tar.gz
-  cd volk-1.3
-  mkdir build && cd build
-  cmake ..
-  make
-else
-  cd volk-1.3/build
-fi
-sudo make install
+#cd ${WORKDIR}
+#rm volk-1.3.tar.gz*
+#if [ ! -d volk-1.3 ]; then
+#  wget http://libvolk.org/releases/volk-1.3.tar.gz
+#  tar -xzf volk-1.3.tar.gz
+#  cd volk-1.3
+#  mkdir build && cd build
+#  cmake ..
+#  make
+#else
+#  cd volk-1.3/build
+#fi
+#sudo make install
 
 cd ${WORKDIR}
 if [ ! -d gnuradio ]; then
@@ -86,10 +64,11 @@ fi
 sudo make install
 
 cd ${WORKDIR}
-rm libsigrokdecode-0.4.1.tar.gz*
-if [ ! -d libsigrokdecode-0.4.1 ]; then
+if [ ! -f libsigrokdecode-0.4.1.tar.gz ]; then
   wget http://sigrok.org/download/source/libsigrokdecode/libsigrokdecode-0.4.1.tar.gz
   tar -xzvf libsigrokdecode-0.4.1.tar.gz
+fi;
+if [ ! -d libsigrokdecode-0.4.1 ]; then
   cd libsigrokdecode-0.4.1
   ./configure
   make
